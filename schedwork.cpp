372bd79a7876c1377a8988a70a4c4ed5a2e8f7a0
Sean Snelder <spsnelde@usc.edu>
2023-04-11 21:32:29 +0000

    completed it mate

--- schedwork.cpp
+++ schedwork.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <set>
 // add or remove necessary headers as you please
 
 #endif
@@ -22,24 +23,118 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool schedulehelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int row_index, int col_index, std::vector<size_t>& num_shifts /*std::set<size_t> workers*/); 
 
+//create 
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
     const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
+    const size_t dailyNeed, //daily need is number of workers needed per day
+    const size_t maxShifts, //max shifts any workers is allowed over n day period
+    DailySchedule& sched //sched has nothing right now
 )
 {
+    //we need to initialise sched, for loop
+    
+    //created by n * d matrix for sched,
+
     if(avail.size() == 0U){
         return false;
     }
     sched.clear();
+    
+    for(int i = 0; i < avail.size(); i++){ //avail.size() = # of days
+        sched.push_back(std::vector<Worker_T>(dailyNeed, -1)); 
+    }
+
+
+
+    //availability matrix to loop through the workers
+    //use data structure need to keep track of # of workers that have worked, increment and decrement vector, numshifts
+    
+    //need to keep track of num shifts, increment once go through for loop, decrement if you return
+    
+    
     // Add your code below
 
+    //avail is an n by k (row =  one day of schedule, by col = one worker's availability) matrix of each of the k workers' availability over n-day period
 
+    //sched is an n by d matrix with d workers IDs who are schedule to work on each of the n days
+    int row_index = 0;
+    int col_index = 0; 
+
+    std::vector<size_t> num_shifts(avail[0].size(), 0); //initialise size to # of workers, and to 0 shifts per worker
+    
+    //std::set<size_t> workers; 
+    return schedulehelper(avail, dailyNeed, maxShifts, sched, row_index, col_index, num_shifts /*workers*/); 
+
+    //schedulehelper(avail, dailyNeed, maxShifts, sched, row_index, col_index, num_shifts, workers); 
+    
+    }
+
+
+
+    
+
+
+
+
+
+
+
+
+bool schedulehelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int row_index, int col_index, std::vector<size_t>& num_shifts/*, std::set<size_t>workers*/){
+
+//std::cout << row_index << " " << avail.size() << std::endl;
+
+if(row_index == avail.size()){ //BC, we pass the final row
+
+    return true; 
+}
+
+
+if(sched[row_index][col_index] == -1){ //hits an invalid ID, we can fill it in
+
+for(size_t i = 0; i < avail[0].size(); i++){ //iterates through the avail columns in singular row
+//check if they are working that day, 
+    if(avail[row_index][i] == 1 /*worker available on specific day*/ /*workers.find(i) == workers.end()*/ /*can't find in set*/  && num_shifts[i] < maxShifts){ //i = worker id (given current row and availability)
+    
+    //workers.insert(i); 
+    num_shifts[i]++; //increment numshifts for user by 1 
+    //recursive call?
+    sched[row_index][col_index] = i; //userID gets filled in
+    //worker 1 is not able to work on day 1
+    bool check = true; 
+        if(col_index == dailyNeed - 1){
+        check = schedulehelper(avail, dailyNeed, maxShifts, sched, row_index + 1, 0, num_shifts/* workers*/);
+
+        }
+        else{
+    check = schedulehelper(avail, dailyNeed, maxShifts, sched, row_index, col_index + 1, num_shifts/*workers*/);
+        } 
+            if(check == true){
+            return true; 
+            }
+
+    //un do everything +set to normal 
+    //workers.erase(i); 
+     num_shifts[i]--;
+    sched[row_index][col_index] = -1;
+
+    }
+    
+
+}
+ 
 
 
 }
+
+return false;
+
+
+}
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,16 +14,115 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordlehelper(int in_index, const std::string& in, const std::string& floating,  std::string alphabet, std::set<std::string>& wordcombos, const std::set<std::string>& dict); 
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
+    const std::string& in, //correct position, i.e --n-
+    const std::string& floating, //floating letters, i.e di
+    const std::set<std::string>& dict /*epresent all valid words in dictionary from comb*/)
 {
-    // Add your code here
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz"; 
+    
+     
+    std::set<std::string> wordcombos;
+    
+    int in_index = 0; 
 
+    
+    wordlehelper(in_index, in, floating, alphabet, wordcombos, dict); 
+    // Add your code here
+    //helper func. , pass in string in , pass in string of alphabet, int length, pass in set of strings to hold combinations
+
+
+    return wordcombos;  
 }
 
 // Define any helper functions here
+
+void wordlehelper(int in_index, const std::string& in, const std::string& floating,  std::string alphabet, std::set<std::string>& wordcombos, const std::set<std::string>& dict){
+    
+    std::string inplace = in; // declare and initialize inplace variable
+    std:: string floatplace = floating;
+    size_t nondashcount = 0; //have to keep track of nondashcount
+
+    if(in_index == in.size()){ //BC, check if we have reached end of word
+      
+      if(dict.find(in) != dict.end() && floating.size() == 0){
+       wordcombos.insert(in);  
+      
+      }
+      return;
+    }
+            
+      for(int i = in_index +1; i < inplace.size(); i++){
+
+            if(inplace.at(i) != '-'){
+              nondashcount++; 
+          }
+          }
+      
+
+    if(inplace.at(in_index) == '-'){ //create an index to go through the "in" function 
+          
+          
+
+          int numblanks = inplace.size() - in_index;
+            
+            std::set<char> usedfloating; 
+
+            for(int i = 0; i < floatplace.size(); i++){ //looping through the floating string
+                 
+                if(usedfloating.find(floatplace.at(i)) != usedfloating.end()){
+                  continue; //if we do find and there is no repetition of letter
+
+
+                }
+                inplace[in_index] = floatplace.at(i); //originally "-",setting floating character to a character in "in"
+                std::string tempfloat = floatplace; 
+                tempfloat.erase(i, 1); 
+
+                
+                wordlehelper(in_index + 1, inplace, tempfloat, alphabet, wordcombos, dict); 
+                //}
+                usedfloating.insert(floatplace.at(i)); 
+
+                inplace[in_index] = '-'; 
+                
+                
+            }
+
+
+            if(numblanks > floatplace.size() + nondashcount){
+                for(char a: alphabet){ //we need a check whether alphabet has floating 
+            //update usedfloating 
+            //secondary updae
+                  if(usedfloating.find(a) != usedfloating.end()){
+               
+              continue; 
+                  }
+
+            inplace.at(in_index) = a; 
+            wordlehelper(in_index + 1, inplace, floatplace, alphabet, wordcombos, dict);
+
+            inplace.at(in_index) = '-'; 
+            
+
+            } 
+            
+          }
+    }
+    
+    else if(inplace.at(in_index) != '-'){ //doesn't go through the 
+
+      wordlehelper(in_index + 1, inplace, floatplace, alphabet, wordcombos, dict); 
+    }
+}
+
+
+
+
+
+
+//How would I approach? 
+
